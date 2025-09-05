a82bafe5252da6799dc8370c3819cdb3acee1617
Bis <bisesh.sitaula@gmail.com>
2025-09-05 15:14:57 +0100

    Lazily hydrate hero section

--- frontend/components/landing/Hero.tsx
+++ frontend/components/landing/Hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Button from "@/components/Button";
 import { colours } from "@/constants";
 import { useGSAP } from "@gsap/react";
@@ -7,14 +8,21 @@ import gsap from "gsap";
 import { SplitText } from "gsap/all";
 import { useRouter } from "next/navigation";
 import MediaQuery, { useMediaQuery } from "react-responsive";
+import { useState, useEffect } from "react";
 
 export default function Hero() {
   const isSmall = useMediaQuery({ maxWidth: 1024 });
   const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   useGSAP(() => {
+    if (!isClient) return;
     const introTl = gsap.timeline();
-    const introSplitText = SplitText.create("#intro-title", {
+    const introSplitText = new SplitText("#intro-title", {
       type: `${isSmall ? "chars,words" : "chars"}`,
     });
     introTl
@@ -65,11 +73,14 @@ export default function Hero() {
         "<"
       )
       .to("#intro-container", { display: "none", duration: 0.25 }, "<");
-  });
+  }, [isClient, isSmall]);
+
   return (
     <div className="relative w-full h-dvh bg-bg p-4">
       <div
-        className="h-screen w-full flex-center relative absolute-center z-1"
+        className={`h-screen w-full flex-center relative absolute-center z-1 ${
+          !isClient ? "block" : ""
+        }`}
         id="intro-container"
       >
         <div
@@ -111,39 +122,41 @@ export default function Hero() {
           playsInline
           className="absolute-center object-cover w-full h-full blur-xs"
         />
-        <div className="gap-8 flex flex-col relative h-dvh pt-20 md:pt-0">
-          <h1 className="text-h1 text-center">WHO&apos;S THAT ACTOR</h1>
-          <h2 className="text-h2 text-center text-text">
-            Find any actor in any <br />
-            movie or show. <br />
-            <MediaQuery maxWidth={768}>
-              <br />
-            </MediaQuery>
-            Instantly. <br />
-            <MediaQuery maxWidth={768}>
-              <br />
-            </MediaQuery>
-            Just take a picture.
-          </h2>
-          <div className="flex-1 items-center flex justify-center mb-10">
-            <Button text="Get Started" onClick={() => router.push("/app")} />
-          </div>
+        {isClient && (
+          <div className="gap-8 flex flex-col relative h-dvh pt-20 md:pt-0">
+            <h1 className="text-h1 text-center">WHO&apos;S THAT ACTOR</h1>
+            <h2 className="text-h2 text-center text-text">
+              Find any actor in any <br />
+              movie or show. <br />
+              <MediaQuery maxWidth={768}>
+                <br />
+              </MediaQuery>
+              Instantly. <br />
+              <MediaQuery maxWidth={768}>
+                <br />
+              </MediaQuery>
+              Just take a picture.
+            </h2>
+            <div className="flex-1 items-center flex justify-center mb-10">
+              <Button text="Get Started" onClick={() => router.push("/app")} />
+            </div>
 
-          <p className="text-alt-text absolute bottom-0 left-0 m-8">
-            Actor Recogniser <br className="md:hidden" />© 2025
-          </p>
-          <div
-            className="absolute bottom-0 md:bottom-10 -right-10 md:right-0"
-            style={{ width: "100%", height: "auto", maxWidth: "200px" }}
-          >
-            <DotLottieReact
-              src={process.env.LOTTIE_SCROLL_URL}
-              loop={true}
-              autoplay={true}
-              style={{ width: "100%", height: "100%" }}
-            />
+            <p className="text-alt-text absolute bottom-0 left-0 m-8">
+              Actor Recogniser <br className="md:hidden" />© 2025
+            </p>
+            <div
+              className="absolute bottom-0 md:bottom-10 -right-10 md:right-0"
+              style={{ width: "100%", height: "auto", maxWidth: "200px" }}
+            >
+              <DotLottieReact
+                src={process.env.LOTTIE_SCROLL_URL}
+                loop={true}
+                autoplay={true}
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
